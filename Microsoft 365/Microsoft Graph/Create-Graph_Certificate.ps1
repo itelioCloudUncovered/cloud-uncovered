64cd8584b3b81702b9132acca7227ea440a52cd1
raphaelb11 <60976251+raphaelb11@users.noreply.github.com>
2023-09-19 08:30:49 +0200

    Add files via upload

---
+++ Microsoft 365/Microsoft Graph/Create-Graph_Certificate.ps1
@@ -0,0 +1,14 @@
+﻿#Name und Exportpfad angeben
+$certname = "Name des Zertifikats"
+$ExportPath = "$env:USERPROFILE\Desktop\$certname.cer"
+
+#Zertifikat erstellen
+$cert = New-SelfSignedCertificate -Subject "CN=$certname" -CertStoreLocation "Cert:\CurrentUser\My" `
+-KeyExportPolicy Exportable -KeySpec Signature -KeyLength 2048 -KeyAlgorithm RSA -HashAlgorithm SHA256
+
+#Zertifikat als Datei exportieren
+Export-Certificate -Cert $cert -FilePath "$ExportPath"
+
+#Subject und Thumbprint auslesen
+$cert.Subject
+$cert.Thumbprint
